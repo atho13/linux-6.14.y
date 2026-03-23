801b7e99d3169e159bdc539fcfef2c248849b20e
Christian Hewitt <christianshewitt@gmail.com>
2026-03-23 05:30:02 +0000

    WIP: media: platform: amlogic: add meson-demux driver
    The meson-demux driver handles demux of signals from demod/tuner
    chips and supports dvb-frontend operations. The driver has been
    adapted from the Amlogic Linux 5.15 vendor kernel.
    
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

---
+++ drivers/media/platform/amlogic/meson-demux/aml_demod_gt.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ *
+ */
+
+#ifndef __AML_DEMOD_GT_H__
+#define __AML_DEMOD_GT_H__
+
+#include "dvb_common.h"
+
+
+#endif /*__AML_DEMOD_GT_H__*/
